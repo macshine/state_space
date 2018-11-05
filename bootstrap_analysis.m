4372afc0110b0506974b7c4a38bf2e53953f7c4f
Mac Shine <macshine@stanford.edu>
2018-11-05 14:18:52 +1100

    Update bootstrap_analysis.m

--- bootstrap_analysis.m
+++ bootstrap_analysis.m
@@ -1,10 +1,14 @@
-for i = 1:100
+nsubs = 100;
+nsubs2 = 200;
+%data_concat = REGIONS x TIME, CONCATENATED ACROSS 200 SUBJECTS (i.e. 375 regions x 368,000 time points)
 
-    rand_vec = rand(200,1);
+for i = 1:nsubs
+
+    rand_vec = rand(nsubs2,1);
     [~,sort_rand] = sort(rand_vec);
-    sub_order = [1:1:200]';
+    sub_order = [1:1:nsubs2]';
     sub_rand = sub_order(sort_rand);
-    sub_rand = sub_rand(1:100,1);
+    sub_rand = sub_rand(1:nsubs,1);
     [sub_rand1,~] = sort(sub_rand);
     
     data_boot = nanmean(data_concat(:,:,sub_rand1),3);
